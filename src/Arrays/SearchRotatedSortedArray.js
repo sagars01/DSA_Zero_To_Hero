58097593eed5f9c9a907a941906af5d68a9468a2
sagars01 <sagarmoysengupta@gmail.com>
2022-03-24 23:31:59 +0530

    2 files added

---
+++ src/Arrays/SearchRotatedSortedArray.js
@@ -0,0 +1,40 @@
+/**
+ * @link https://leetcode.com/problems/search-in-rotated-sorted-array/
+ * @example Input: nums = [4,5,6,7,0,1,2], target = 0
+            Output: 4
+ * @implements You must write an algorithm with O(log n) runtime complexity.
+ * @type Medium
+ * @author sagars01
+ */
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function (nums, target) {
+  let start = 0;
+  let end = nums.length - 1;
+
+  if (nums.length === 1 && nums[0] !== target) return -1;
+  // [5 , 1 , 2 ] 5
+  while (start <= end) {
+    const mid = Math.floor((start + end) / 2);
+    if (nums[mid] === target) return mid;
+
+    if (nums[start] < nums[mid]) {
+      if (target <= nums[mid] && target >= nums[start]) {
+        end = mid - 1;
+      } else {
+        start = mid + 1;
+      }
+    } else {
+      if (target <= nums[end] && target >= nums[mid]) start = mid + 1;
+      else end = mid - 1;
+    }
+  }
+
+  return -1;
+};
+
+module.exports = search;
