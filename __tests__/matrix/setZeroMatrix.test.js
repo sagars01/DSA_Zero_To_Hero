373dc2e1855126ba410212d02edc93650db669df
sagars01 <sagarmoysengupta@gmail.com>
2022-04-07 11:01:28 +0530

    Lint

--- __tests__/matrix/setZeroMatrix.test.js
+++ __tests__/matrix/setZeroMatrix.test.js
@@ -20,7 +20,11 @@ describe('Matrix: SetZeroMatrix Test Suite', () => {
       [3, 4, 5, 2],
       [1, 3, 1, 5],
     ];
-    let o = [[0,0,0,0],[0,4,5,0],[0,3,1,0]];
+    let o = [
+      [0, 0, 0, 0],
+      [0, 4, 5, 0],
+      [0, 3, 1, 0],
+    ];
     expect(ZeroMatrix(matrix)).toEqual(o);
   });
 });
