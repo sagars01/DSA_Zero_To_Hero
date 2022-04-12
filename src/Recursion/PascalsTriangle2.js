a7a7c24a612028f2594aceb1aa9c38399e2aaa8d
sagars01 <sagarmoysengupta@gmail.com>
2022-04-12 09:50:32 +0530

    Pascal's Triangle 2

--- src/Recursion/PascalsTriangle2.js
+++ src/Recursion/PascalsTriangle2.js
@@ -6,7 +6,43 @@
  * @param {number} rowIndex
  * @return {number[]}
  */
-var f = function (i, j) {
-  if (i === 0) return [1];
-  const sol = f(i - 1, j - 1) + f(i - 1, j);
+const pascalsTriangle = (rowIndex) => {
+  // This implementation is quadratic time and should be avoided at all cost.
+  const getNums = (row, col) => {
+    if (row === 0 || col === 0 || row === col) return 1;
+
+    return getNums(row - 1, col - 1) + getNums(row - 1, col);
+  };
+
+  let pascal = [];
+  for (let i = 0; i <= numRows; i++) {
+    pascal.push(getNums(rowIndex, i));
+  }
+
+  return pascal;
+};
+
+/**
+ * @description time optimized solution O(n)
+ * @link https://leetcode.com/problems/pascals-triangle-ii/discuss/1203260/Very-easy-O(N)-time.-0-ms-beats-100-(simple-maths)-ALL-LANGUAGES
+ *
+ * Brilliant Solution by the user
+ *
+ */
+
+const pascalTriangleOptimized = (rowIndex) => {
+  let pascal = [1];
+  let temp = 1;
+
+  for (let i = 1; i <= rowIndex; i++) {
+    temp = (temp * (rowIndex - i)) / i;
+    pascal.push(temp);
+  }
+
+  return temp;
+};
+
+module.exports = {
+  pascalsTriangle,
+  pascalTriangleOptimized,
 };
