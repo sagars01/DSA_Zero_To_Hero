507e6c4edf7a74e41f6f51576350b32482800ed5
sagars01 <sagarmoysengupta@gmail.com>
2022-04-17 18:21:50 +0530

    N - Queens Description

---
+++ src/Backtracking/N-Queens.js
@@ -0,0 +1,10 @@
+/**
+ * Solution for the N-Queens Problem
+ * @link https://leetcode.com/problems/n-queens/
+ * @author sagars01
+ * @example Brief Explanation
+ * Input: n = 4
+  Output: [[".Q..","...Q","Q...","..Q."],["..Q.","Q...","...Q",".Q.."]]
+  Explanation: There exist two distinct solutions to the 4-queens puzzle as shown above
+ *
+ */
