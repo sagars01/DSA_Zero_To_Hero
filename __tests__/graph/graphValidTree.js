6b0c3ffe244c9356d5f2be2a4d64d8f9ac81cb72
sagars01 <sagarmoysengupta@gmail.com>
2022-05-02 09:01:58 +0530

    Graph: Is Valid Tree

---
+++ __tests__/graph/graphValidTree.js
@@ -0,0 +1,14 @@
+const GVT = require('../../src/Graph/GraphValidTree');
+
+describe('Graph: Valid Tree Suite', () => {
+  test('should be true as graph is a valid tree', () => {
+    const n = 5,
+      edges = [
+        [0, 1],
+        [0, 2],
+        [0, 3],
+        [1, 4],
+      ];
+    expect(GVT(n, edges)).toBe(tr);
+  });
+});
