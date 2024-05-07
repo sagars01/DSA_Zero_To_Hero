53f2a98a6c79d8dfc4a55985104dff3c3a90216c
Sagar <sagarmoysengupta@gmail.com>
2024-05-07 09:42:26 +0530

    to code reverse vowels using maps

---
+++ Go/Grokking-The-Coding-Interview/01_WarmUp/reverse_vowels.go
@@ -0,0 +1,53 @@
+/**
+Given a string s, reverse only all the vowels in the string and return it.
+
+The vowels are 'a', 'e', 'i', 'o', and 'u', and they can appear 
+in both lower and upper cases, more than once.
+
+Example 1:
+
+Input: s= "hello"
+Output: "holle"
+
+*/
+
+import ( )
+
+// Solution class equivalent in Go
+type Solution struct{}
+
+
+func (s *Solution) reverseVowels(str string) string {
+	slice := []rune(str) // Converts the string to array
+	vowelSet := map[rune]bool{ 
+		'a' : true,
+		'e' : true,
+		'i' : true,
+		'o' : true,
+		'u' : true,
+		'A': true,
+		'E': true,
+		'I': true,
+		'O': true,
+		'U': true}
+	start := 0 
+	end := len(slice) - 1
+
+	for start < end {
+		for start < end && !vowelSet[slice[start]] {
+			start++
+		}
+
+		for start < end && !vowelSet[slice[end]] {
+			end--
+		}
+
+		if start < end {
+			slice[start] , slice[end] = slice[end] , slice[start]
+			start++
+			end--
+		}
+	}
+	return string(slice) 
+}
+
