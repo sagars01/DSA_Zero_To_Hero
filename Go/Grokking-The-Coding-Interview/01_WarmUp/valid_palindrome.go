ddda86eaf143270ec8df658e1779a5428bdb1906
Sagar <sagarmoysengupta@gmail.com>
2024-05-08 11:25:02 +0530

    to code valid palindrome

---
+++ Go/Grokking-The-Coding-Interview/01_WarmUp/valid_palindrome.go
@@ -0,0 +1,51 @@
+package warmup
+
+import (
+    "unicode"
+    "strings"
+	
+)
+
+type Solution struct {
+}
+
+
+func (sol *Solution) isPalindrome(s string) bool {
+
+    start := 0
+    end := len(s) - 1
+
+ 
+    for start < end {
+
+        for start < end && !isLetterOrDigit(rune(s[start])) {
+            start++
+        }
+
+        for start < end && !isLetterOrDigit(rune(s[end])) {
+            end--
+        }
+        
+
+        if strings.ToLower(string(s[start])) != strings.ToLower(string(s[end])) {
+			return false 
+		}
+
+        start++
+        end--
+    }
+
+    return true
+}
+
+func isLetterOrDigit(r rune) bool {
+	return unicode.IsLetter(r) || unicode.IsDigit(r)
+}
+
+/**
+Golang Learning
+
+append returns a new slice: so assign it back to the original variable
+rune can be validate using unicode
+
+*/
